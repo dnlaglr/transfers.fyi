9271e5f17aafbb75234c85bc337c3e63e120e246
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-03-10 18:47:27 -0700

    Utilize prettier in project

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,20 +3,43 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className='flex flex-row justify-center items-center w-screen px-12 py-4'>
+    <nav className='flex w-screen flex-row items-center justify-center px-12 py-4'>
       <Link href='/' className='flex items-center pr-6'>
-        <Image src='/assets/transfers.svg' width={32} height={32} alt='Transfers.fyi Logo' />
-        <h1 className='text-2xl text-text-main font-bold ml-2'>Transfers.fyi</h1>
+        <Image
+          src='/assets/transfers.svg'
+          width={32}
+          height={32}
+          alt='Transfers.fyi Logo'
+        />
+        <h1 className='ml-2 text-2xl font-bold text-text-main'>
+          Transfers.fyi
+        </h1>
       </Link>
-      <div className='flex flex-row justify-between items-center flex-1'>
-        <div className='text-base text-text-main font-semibold'>
-          <Link href='/resources' className='mr-4'>Resources</Link>
-          <Link href='/colleges' className='mr-4'>Colleges</Link>
-          <Link href='/compare' className='mr-4'>Compare</Link>
+      <div className='flex flex-1 flex-row items-center justify-between'>
+        <div className='text-base font-semibold text-text-main'>
+          <Link href='/resources' className='mr-4'>
+            Resources
+          </Link>
+          <Link href='/colleges' className='mr-4'>
+            Colleges
+          </Link>
+          <Link href='/compare' className='mr-4'>
+            Compare
+          </Link>
         </div>
-        <div className='text-base text-text-main font-semibold'>
-          <Link href='/signin' className='border border-slate-300 hover:bg-neutral-100 px-4 py-2 mr-4 rounded-xl'>Sign In</Link>
-          <Link href='/dashboard' className='text-white bg-text-main hover:bg-black px-4 py-2 rounded-xl'>Dashboard</Link>
+        <div className='text-base font-semibold text-text-main'>
+          <Link
+            href='/signin'
+            className='mr-4 rounded-xl border border-slate-300 px-4 py-2 hover:bg-neutral-100'
+          >
+            Sign In
+          </Link>
+          <Link
+            href='/dashboard'
+            className='rounded-xl bg-text-main px-4 py-2 text-white hover:bg-black'
+          >
+            Dashboard
+          </Link>
         </div>
       </div>
     </nav>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 import Layout from './_layout';
 
 export default function App({ Component, pageProps }: AppProps) {
