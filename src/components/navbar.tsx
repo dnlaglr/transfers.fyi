ac6aee8aadadbbe15dccfcc94032ba6d92783d1b
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-03-12 01:38:55 -0700

    Fix overflow issue

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className='flex w-screen flex-row items-center justify-center px-12 py-4'>
+    <nav className='flex w-full flex-row items-center justify-center px-12 py-4'>
       <Link href='/' className='flex items-center pr-6'>
         <Image
           src='/assets/transfers.svg'
