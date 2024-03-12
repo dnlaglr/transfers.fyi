5e761e0e971c8711b947a227f766116316156097
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-03-12 01:59:05 -0700

    Fix react/no-unescaped-entities error

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ export default function Error404() {
   return (
     <div className='flex flex-col items-center w-full h-[calc(100vh_-_64px)]'>
       <h1 className='text-8xl text-text-main font-bold mt-48'>404</h1>
-      <h2 className='text-xl mt-8'>This link doesn't exist or this feature might be undergoing maintenance!</h2>
+      <h2 className='text-xl mt-8'>This link doesn&apos;t exist or this feature might be undergoing maintenance!</h2>
       <h3 className='text-xl mt-2'>Sorry for the inconvenience!</h3>
     </div>
   )
